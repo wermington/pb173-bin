8b2591dd8bffcb0b196e8906f2fd081b0b4899d1
Jiri Slaby <jirislaby@gmail.com>
2013-11-04 20:52:21 +0100

    07: add

---
+++ 07/nostd.c
@@ -0,0 +1,33 @@
+#include <stdarg.h>
+#include <asm/unistd.h>
+
+static __attribute((noinline,noclone)) long syscall(unsigned int number, ...)
+{
+	long ret;
+#ifdef __x86_64__
+	asm volatile(
+		"movq %%rsi, %%rdi\n"
+		"movq %%rdx, %%rsi\n"
+		"movq %%rcx, %%rdx\n"
+		"movq %%r8, %%r10\n"
+		"movq %%r9, %%r8\n"
+		"movq 8(%%rsp),%%r9\n"
+		"syscall" : "=a" (ret) : "a" (number) : "r10", "rdi", "rsi", "rdx", "r8");
+#else
+	asm volatile(
+		"movl 8(%%esp), %%ebx\n"
+		"movl 12(%%esp), %%ecx\n"
+		"movl 16(%%esp), %%edx\n"
+		"movl 20(%%esp), %%esi\n"
+		"movl 24(%%esp), %%edi\n"
+		"movl 28(%%esp), %%ebp\n"
+		"int $0x80" : "=a" (ret) : "a" (number));
+#endif
+	return ret;
+}
+
+void start()
+{
+	char buf[16];
+	syscall(__NR_read, 0, buf, 16);
+}
