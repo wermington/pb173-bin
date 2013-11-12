e7a01c4d6349d36816ad02b747264b759adc5219
Jiri Slaby <jirislaby@gmail.com>
2013-11-12 13:53:38 +0100

    08: remove rep from nops

--- 08/prof.c
+++ 08/prof.c
@@ -6,7 +6,7 @@ static __attribute__((noinline)) void fun3(void)
 	unsigned int a;
 
 	for (a = 0; a < 300000; a++)
-		asm volatile("rep nop" ::: "memory");
+		asm volatile("nop" ::: "memory");
 }
 
 static __attribute__((noinline)) void fun2(void)
@@ -14,7 +14,7 @@ static __attribute__((noinline)) void fun2(void)
 	unsigned int a;
 
 	for (a = 0; a < 100000; a++)
-		asm volatile("rep nop" ::: "memory");
+		asm volatile("nop" ::: "memory");
 }
 
 static __attribute__((noinline)) void fun1(void)
