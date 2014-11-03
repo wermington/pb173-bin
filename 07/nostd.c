46daa88f00b8d55379205fe99313addc4e50f420
Jiri Slaby <jirislaby@gmail.com>
2014-11-03 20:02:52 +0100

    07: proper syscal regs

--- 07/nostd.c
+++ 07/nostd.c
@@ -1,30 +1,36 @@
 #include <stdarg.h>
 #include <asm/unistd.h>
 
-static __attribute((noinline,noclone)) long syscall(unsigned int number, ...)
-{
-	long ret;
 #ifdef __x86_64__
-	asm volatile(
-		"movq %%rsi, %%rdi\n"
-		"movq %%rdx, %%rsi\n"
-		"movq %%rcx, %%rdx\n"
-		"movq %%r8, %%r10\n"
-		"movq %%r9, %%r8\n"
-		"movq 8(%%rsp),%%r9\n"
-		"syscall" : "=a" (ret) : "a" (number) : "r10", "rdi", "rsi", "rdx", "r8");
+# define ASM_LINKAGE
 #else
-	asm volatile(
-		"movl 8(%%esp), %%ebx\n"
-		"movl 12(%%esp), %%ecx\n"
-		"movl 16(%%esp), %%edx\n"
-		"movl 20(%%esp), %%esi\n"
-		"movl 24(%%esp), %%edi\n"
-		"movl 28(%%esp), %%ebp\n"
-		"int $0x80" : "=a" (ret) : "a" (number) : "ebx", "ecx", "edx", "esi", "edi", "ebp");
+# define ASM_LINKAGE __attribute__((regparm(0)))
 #endif
-	return ret;
-}
+
+long ASM_LINKAGE syscall(unsigned int number, ...);
+
+asm(
+	"syscall:\n"
+#ifdef __x86_64__
+	"movq %rdi, %rax\n"
+	"movq %rsi, %rdi\n"
+	"movq %rdx, %rsi\n"
+	"movq %rcx, %rdx\n"
+	"movq %r8, %r10\n"
+	"movq %r9, %r8\n"
+	"movq 8(%rsp), %r9\n"
+	"syscall\n"
+#else
+	"movl 4(%esp), %eax\n"
+	"movl 8(%esp), %ebx\n"
+	"movl 12(%esp), %ecx\n"
+	"movl 16(%esp), %edx\n"
+	"movl 20(%esp), %esi\n"
+	"movl 24(%esp), %edi\n"
+	"movl 28(%esp), %ebp\n"
+	"int $0x80\n"
+#endif
+	"ret\n");
 
 void start()
 {
