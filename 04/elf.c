4f48e059f4bc477550e1eebbbb5a09f148768797
Jiri Slaby <jirislaby@gmail.com>
2015-10-14 22:21:34 +0200

    04: add
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ 04/elf.c
@@ -0,0 +1,20 @@
+#include <ctype.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <libelf.h>
+#include <gelf.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+int main(int argc, char **argv)
+{
+	/* Elf *elf; */
+
+	return 0;
+}
