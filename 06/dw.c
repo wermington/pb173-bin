72b1a24b2aab5f2ff3b12841760fe0ceb93a6380
Jiri Slaby <jirislaby@gmail.com>
2015-11-03 18:50:38 +0100

    06: add skeleton
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

---
+++ 06/dw.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include <err.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <elfutils/libdw.h>
+#include <elfutils/libdwfl.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+
+static const Dwfl_Callbacks cb = {
+};
+
+int main(int argc, char **argv)
+{
+	/* Dwfl *dwfl; */
+
+	return 0;
+}
