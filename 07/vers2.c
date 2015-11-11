069b998ae332175baa0450b5740162b957d0655a
Jiri Slaby <jirislaby@gmail.com>
2015-11-11 16:02:52 +0100

    07: better names for funs
    Signed-off-by: Jiri Slaby <jirislaby@gmail.com>

--- 07/vers2.c
+++ 07/vers2.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void xy_ver1(void)
+void bubak_ver1(void)
 {
 	puts("O1 COMPAT");
 }
-asm(".symver xy_ver1,bubak@");
+asm(".symver bubak_ver1,bubak@");
 
-void xy_ver2(void)
+void bubak_ver2(void)
 {
 	puts("O2");
 }
-asm(".symver xy_ver2,bubak@@VERS_1.1");
+asm(".symver bubak_ver2,bubak@@VERS_1.1");
